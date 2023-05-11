abb9ea4092ec949463f0d8434fb7602876d9cb0b
u958189 <abouazza@pca.ma>
2023-05-11 11:39:35 +0100

    update: information

--- index.js
+++ index.js
@@ -2,22 +2,79 @@
 
 "use strict";
 
-const boxen = require("boxen");
-const chalk = require("chalk");
-const inquirer = require("inquirer");
-const clear = require("clear");
-const open = require("open");
+import terminalImage from "terminal-image";
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import clear from "clear";
+import open from "open";
 
 clear();
 
-const prompt = inquirer.createPromptModule();
+const colors = {
+  primary: "#a95fed",
+};
 
+const data = {
+  name: "Bouazza Ayyoub",
+  whoIAm:
+    "Excited to Build Software Products and Help Others Learn Computer Science.",
+  linkedIn: "https://www.linkedin.com/in/bouazza-ayyoub",
+  twitter: "https://twitter.com/AyyoubBouazza",
+  github: "https://github.com/bouazzaayyoub",
+  medium: "https://medium.com/@bouazzaayyoub",
+  whatIamDoing:
+    "I will help you build software products and learn computer science.",
+};
+
+// Build the card
+const me = boxen(
+  [
+    `${chalk.hex(colors.primary).bold(data.name)}`,
+    ``,
+    `${chalk.white.bold("Software Engineer")}`,
+    `${data.whoIAm}`,
+    ``,
+    `${chalk.hex(colors.primary).bold("Contact:")}`,
+    `${chalk.white.bold("LinkedIn:")}  ${chalk.underline(data.linkedIn)}`,
+    `${chalk.white.bold("Medium:")}    ${chalk.underline(data.medium)}`,
+    `${chalk.white.bold("Github:")}    ${chalk.underline(data.github)}`,
+    `${chalk.white.bold("Twitter:")}   ${chalk.underline(data.twitter)}`,
+    ``,
+    `${chalk.hex(colors.primary).bold("About me:")}`,
+    `${chalk.italic(data.whatIamDoing)}`,
+    ``,
+    `${chalk.italic("Let's talk")} ${chalk.hex(colors.primary)(":)")}`,
+  ].join("\n"),
+  {
+    margin: 1,
+    float: "center",
+    padding: 2,
+    borderStyle: "round",
+    borderColor: colors.primary,
+  }
+);
+
+// Print the card
+console.log(me);
+
+// Optional tip to help users use the links
+const tip = [
+  `Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above`,
+  "",
+].join("\n");
+
+// Show the tip
+console.log(tip);
+
+// Ask the Inquirer questions.
+const prompt = inquirer.createPromptModule();
 // Questions after the card
 const questions = [
   {
     type: "list",
     name: "action",
-    message: "Do you have something to told me?",
+    message: "Do you want to build a software product?",
     choices: [
       {
         name: `Send me an ${chalk.green.bold("email")}?`,
@@ -35,72 +92,4 @@ const questions = [
     ],
   },
 ];
-
-// Data for the card
-const data = {
-  name: chalk.bold.green("Bouazza Ayyoub"),
-  iam: `${chalk.white("Software Engineer Student")} ${chalk
-    .hex("#2b82b2")
-    .bold()}`,
-  linkedin:
-    chalk.gray("https://www.linkedin.com/in/") + chalk.blue("bouazza-ayyoub"),
-  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("AyyoubBouazza"),
-  github: chalk.gray("https://github.com/") + chalk.green("bouazzaayyoub"),
-
-  school:
-    chalk.white("The High National School of Mines in Rabat-") +
-    chalk.yellow("ENSMR"),
-  npx: chalk.red("npx") + " " + chalk.white("ayyoubouazza"),
-
-  labelIam: chalk.white.bold("I am a:"),
-  labelTwitter: chalk.white.bold("Twitter:"),
-  labelGitHub: chalk.white.bold("GitHub:"),
-  labelLinkedIn: chalk.white.bold("LinkedIn:"),
-  labelSchool: chalk.white.bold("School:"),
-  labelCard: chalk.white.bold("Card:"),
-};
-
-// Build the card
-const me = boxen(
-  [
-    `${""}                    ${data.name}`,
-    ``,
-    `${data.labelIam}   ${data.iam}`,
-    `${data.labelSchool}   ${data.school}`,
-    `${data.labelLinkedIn} ${data.linkedin}`,
-    `${data.labelGitHub}   ${data.github}`,
-    `${data.labelTwitter}  ${data.twitter}`,
-    ``,
-    `${data.labelCard}     ${data.npx}`,
-    ``,
-    `${chalk.italic(
-      "I'm a developer, ENIM Lover and student most of the time."
-    )}`,
-    `${chalk.italic(
-      "I aspire to inspire others and to change my environment."
-    )}`,
-    `${chalk.italic("Let's talk | Let's work together")} ${chalk.yellow(":)")}`,
-  ].join("\n"),
-  {
-    margin: 1,
-    float: "center",
-    padding: 1,
-    borderStyle: "round",
-    borderColor: "green",
-  }
-);
-
-// Print the card
-console.log(me);
-
-// Optional tip to help users use the links
-const tip = [
-  `Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above`,
-  "",
-].join("\n");
-
-// Show the tip
-console.log(tip);
-
-// Ask the Inquirer questions.
 prompt(questions).then((answer) => answer.action());
